5f24d882b8ce9bc95a7be87c9a4da384d2941e2d
Sean McPherson <seanmcp25@gmail.com>
2017-07-29 13:37:25 -0400

    Style and function; added favicon

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById("screen");
+let startVal = document.getElementById("startVal");
+
+console.log(startVal.value);
+
+let screenValue = 0;
+screen.innerHTML = screenValue;
+
+plusOne.addEventListener('click', function(){
+  screenValue++;
+  screen.innerHTML = screenValue;
+});
+
+minusOne.addEventListener('click', function(){
+  screenValue--;
+  screen.innerHTML = screenValue;
+})
+
+resetVal.addEventListener('click', function(){
+  screenValue = 0;
+  screen.innerHTML = screenValue;
+  startVal.value = "";
+})
+
+startValButton.addEventListener('click', function(){
+  let newStartVal = startVal.value;
+  screenValue = newStartVal;
+  startVal.value = "";
+  screen.innerHTML = screenValue;
+});
