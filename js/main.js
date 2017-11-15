3f8956705fbe35c9cf6301f38863bc2cc2809bde
Sean McPherson <seanmcp25@gmail.com>
2017-11-15 10:21:24 -0500

    Remove log; create single function to increment counter

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
-let screen = document.getElementById("screen");
-let startVal = document.getElementById("startVal");
-
-console.log(startVal.value);
+let output = document.getElementById("screen");
+let initial = document.getElementById("startVal");
 
 let screenValue = 0;
-screen.innerHTML = screenValue;
+output.innerHTML = screenValue;
+
+increment = action => {
+  if (action === 'plus') {
+    screenValue += 2;
+  }
+  screenValue--;
+  output.textContent = screenValue;
+}
 
 plusOne.addEventListener('click', function(){
   screenValue++;
-  screen.innerHTML = screenValue;
+  output.innerHTML = screenValue;
 });
 
 minusOne.addEventListener('click', function(){
   screenValue--;
-  screen.innerHTML = screenValue;
+  output.innerHTML = screenValue;
 })
 
 resetVal.addEventListener('click', function(){
   screenValue = 0;
-  screen.innerHTML = screenValue;
-  startVal.value = "";
+  output.innerHTML = screenValue;
+  initial.value = "";
 })
 
 startValButton.addEventListener('click', function(){
-  let newStartVal = startVal.value;
+  let newStartVal = initial.value;
   if (newStartVal) {
     screenValue = newStartVal;
-    startVal.value = "";
-    screen.innerHTML = screenValue;
+    initial.value = "";
+    output.innerHTML = screenValue;
   }
 });
