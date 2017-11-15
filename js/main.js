2e0f03f72d38f925863c34282e4cb4b3364d68df
Sean McPherson <seanmcp25@gmail.com>
2017-11-15 10:25:44 -0500

    Create function to reset screen; use onclick for screen buttons

--- js/main.js
+++ js/main.js
@@ -5,28 +5,18 @@ let screenValue = 0;
 output.innerHTML = screenValue;
 
 increment = action => {
-  if (action === 'plus') {
+  if (action === '+') {
     screenValue += 2;
   }
   screenValue--;
   output.textContent = screenValue;
 }
 
-plusOne.addEventListener('click', function(){
-  screenValue++;
-  output.innerHTML = screenValue;
-});
-
-minusOne.addEventListener('click', function(){
-  screenValue--;
-  output.innerHTML = screenValue;
-})
-
-resetVal.addEventListener('click', function(){
+reset = () => {
   screenValue = 0;
   output.innerHTML = screenValue;
   initial.value = "";
-})
+}
 
 startValButton.addEventListener('click', function(){
   let newStartVal = initial.value;
