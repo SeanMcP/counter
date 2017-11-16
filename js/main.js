e78b93aa17345103faf6a874ea9a8e3814d34ad9
Sean McPherson <seanmcp25@gmail.com>
2017-11-16 15:38:05 -0500

    Add local storage to persist count

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
 let output = document.getElementById('screen');
 let initial = document.getElementById('startVal');
 
-// Use Number() to convert numbered strings to numbers
+// Checks for for localStorage before initializing output screen value
+if (localStorage) {
+  output.value = localStorage.count;
+} else {
+  output.value = 0;
+}
 
+// Number() required to convert string to number
 let screenValue = Number(output.value);
+
+updateStorage = () => {
+  localStorage.count = Number(screenValue);
+}
+printOutput = () => {
+  output.value = localStorage.count ? localStorage.count : screenValue;
+}
+screenChange = () => {
+  screenValue = Number(output.value);
+  updateStorage();
+}
 
 increment = action => {
   screenValue = Number(output.value);
   if (action === '+') {
     screenValue += 2;
+    updateStorage();
   }
   screenValue--;
-  output.value = screenValue;
-}
+  updateStorage();
+  printOutput();
+};
 
 reset = () => {
   screenValue = 0;
-  output.value = screenValue;
+  updateStorage();
+  printOutput();
   initial.value = null;
 }
 
@@ -24,7 +44,8 @@ startAt = () => {
   let newStartVal = Number(initial.value);
   if (newStartVal) {
     screenValue = newStartVal;
-    output.value = screenValue;
+    updateStorage();
+    printOutput();
     initial.value = null;
   }
 };
